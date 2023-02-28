84a07d8c726ad5e6be36f52bbaae1a2f022f9ece
Ben Pepper <benpepper@RN9TWMY3WC.local>
2023-02-28 07:33:06 -0700

    Initial commit.

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ben Pepper on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
